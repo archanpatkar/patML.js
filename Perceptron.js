763f9e4243e3a72a1cc107c346fb9220fd0374e7
Archan Patkar <archanpatkar@gmail.com>
2018-02-06 18:47:56 +0530

    Update Perceptron.js

--- Perceptron.js
+++ Perceptron.js
@@ -53,7 +53,7 @@ class Perceptron
 
         this.learing_rate = learing_rate;
 
-        for(let i = 0; i < weights; i++)
+        for(let i = 0; i < weights + 1; i++)
         {
             this.weights.push(random(-1,1));
         }
@@ -73,6 +73,14 @@ class Perceptron
             this.weights[i] += error * inputs[i] * this.learing_rate;
         }
     }
+
+    predict(x)
+    {
+        let w0 = this.weights[0];
+        let w1 = this.weights[1];
+        let wb = this.weights[2];
+        return - (w2/w1) - (w0/w1) * x;
+    }
 }
 
 let p1 = new Perceptron(2,0.01)
@@ -89,7 +97,7 @@ let wrong = 0;
 
 for(let p of points)
 {
-    let inputs = [ p.x , p.y ];
+    let inputs = [ p.x , p.y , 1];
 
     let guess = p1.guess(inputs);
 
@@ -112,7 +120,7 @@ wrong = 0;
 
 for(let p of points)
 {
-    let inputs = [ p.x , p.y ];
+    let inputs = [ p.x , p.y , 1];
     p1.train(inputs , p.label);
 
     let guess = p1.guess(inputs);
@@ -129,3 +137,5 @@ for(let p of points)
 
 console.log("Correct = " + correct);
 console.log("Wrong = " + wrong);
+
+console.log(p1);
